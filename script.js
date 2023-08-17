375828124edf2962ed1540a0ef56a97020a60284
Alan Sobchacki <a.sobchack@gmail.com>
2023-08-16 21:18:11 -0300

    implement very basic graphics and operator functions for the calculator.

---
+++ script.js
@@ -0,0 +1,23 @@
+let currentTotal = 0;
+
+function sum(num1, num2) {
+    let sumTotal = num1 + num2
+    return sumTotal;    
+}
+
+function subtract(num1, num2) {
+    let subtractTotal = num1 - num2
+    return subtractTotal;
+}
+
+function multiply(num1, num2) {
+    let multiplyTotal = num1 * num2;
+    return multiplyTotal;
+}
+
+function divide(num1, num2) {
+    let divideTotal = num1 / num2;
+    return divideTotal;
+}
+
+console.log(divide(1, 2));

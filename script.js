df2ac7d527d8ff30a7c10a995c31705b2519e004
Alan Sobchacki <a.sobchack@gmail.com>
2023-08-17 17:42:32 -0300

    Implement functional calculus for adding, subtracting, multiplying, and dividing integers

--- script.js
+++ script.js
@@ -1,17 +1,120 @@
-let firstNumber = "";
-let secondNumber = "";
-let choseOperator = false;
-let calculus = [];
+// stuff to solve
+// 1 - cannot add more than one 'dot' to a number
+// 2 - 'zero' cannot be added multiple times as a first number (i.e, you cannot enter '0000', but you can enter '8000)
+// 3 -  add a backspace button that erases the last value of the current string (i.e, '72' becomes '7')
+// 4 -  add a C button (clear) that erases everything and resets the calculator to its default settings
+// 5 -  add a CE button (clear entry) that erases the entire, most recent entry
+// 6 -  add a equal buttont that returns the value of the operation
+
+let previousNumber = "";
+let currentNumber = "";
+let finalCalculus = "";
+let isOperatorSelected = false;
 
 function insertNumber(number) {
-    if (choseOperator == false) {
-        firstNumber = firstNumber + number;
-        document.getElementById("display").innerHTML = firstNumber;
+    if (isOperatorSelected == false) {
+        previousNumber = previousNumber + number;
+        document.getElementById("display-bottom").innerHTML = previousNumber;
     } else {
-        secondNumber = secondNumber + number;
-        document.getElementById("display").innerHTML = secondNumber;
+        currentNumber = currentNumber + number;
+        document.getElementById("display-bottom").innerHTML = currentNumber;
     }
 }
+
+function calculate(operator) {
+    isOperatorSelected = true;
+
+    if (operator === 'sum') {
+        let previous = Number(previousNumber);
+        let current = Number(currentNumber);
+        let calculus = previous + current;
+        previousNumber = String(calculus);
+        document.getElementById("display-top").innerHTML = previousNumber + ' + ';
+        finalCalculus = 'sum';
+    } else if (operator === 'minus') {
+        let previous = Number(previousNumber);
+        let current = Number(currentNumber);
+        let calculus = previous - current;
+        previousNumber = String(calculus);
+        document.getElementById("display-top").innerHTML = previousNumber + ' - ';
+        finalCalculus = 'minus';
+    } else if (operator === 'times') {
+        let previous = Number(previousNumber);
+        let current = Number(currentNumber);
+        if (current != 0) {
+            let calculus = previous * current;
+            previousNumber = String(calculus);
+        }
+        document.getElementById("display-top").innerHTML = previousNumber + ' * ';
+        finalCalculus = 'times';
+    } else if (operator === 'divide') {
+        let previous = Number(previousNumber);
+        let current = Number(currentNumber);
+        if (current != 0) {
+            let calculus = previous / current;
+            previousNumber = String(calculus);
+        }
+        document.getElementById("display-top").innerHTML = previousNumber + ' / ';
+        finalCalculus = 'divide';
+    }
+
+    document.getElementById("display-bottom").innerHTML = previousNumber;
+    currentNumber = "";
+}
+
+//calculator operator buttons
+const buttonSum = document.querySelector('#plus');
+buttonSum.addEventListener('click', () => {
+    document.getElementById("display-top").innerHTML = previousNumber + ' + ';
+    calculate('sum');
+})
+
+const buttonMinus = document.querySelector('#minus');
+buttonMinus.addEventListener('click', () => {
+    document.getElementById("display-top").innerHTML = previousNumber + ' - ';
+    calculate('minus');
+})
+
+const buttonTimes = document.querySelector('#times');
+buttonTimes.addEventListener('click', () => {
+    document.getElementById("display-top").innerHTML = previousNumber + ' * ';
+    calculate('times');
+})
+
+const buttonDivide = document.querySelector('#divide');
+buttonDivide.addEventListener('click', () => {
+    document.getElementById("display-top").innerHTML = previousNumber + ' / ';
+    calculate('divide');
+})
+
+const buttonEqual = document.querySelector('#equal');
+buttonEqual.addEventListener('click', () => {
+    if (finalCalculus === 'sum') {
+        let previousTop = previousNumber;
+        let currentTop = currentNumber;
+        calculate('sum');
+        document.getElementById("display-top").innerHTML = previousTop + ' + ' + currentTop + ' = ';
+        console.log(finalCalculus);
+    } else if (finalCalculus === 'minus') {
+        let previousTop = previousNumber;
+        let currentTop = currentNumber;
+        calculate('minus');
+        document.getElementById("display-top").innerHTML = previousTop + ' - ' + currentTop + ' = ';
+        console.log(finalCalculus);
+    } else if (finalCalculus === 'times') {
+        let previousTop = previousNumber;
+        let currentTop = currentNumber;
+        calculate('times');
+        document.getElementById("display-top").innerHTML = previousTop + ' * ' + currentTop + ' = ';
+        console.log(finalCalculus);
+    } else if (finalCalculus === 'divide') {
+        let previousTop = previousNumber;
+        let currentTop = currentNumber;
+        calculate('divide');
+        document.getElementById("display-top").innerHTML = previousTop + ' / ' + currentTop + ' = ';
+        console.log(finalCalculus);
+    }
+})
 
 //calculator number buttons
 const buttonOne = document.querySelector('#one');
@@ -59,22 +162,12 @@ buttonNine.addEventListener('click', () => {
     insertNumber("9");
 });
 
-//calculator operator buttons
-const buttonSum = document.querySelector('#plus');
-buttonSum.addEventListener('click', () => {
-    if (choseOperator == false && calculus.length < 1) {
-        let initialValue = Number(firstNumber); //the string becomes a number
-        calculus.unshift(initialValue); //that number is pushed into the beginning of our array, which has an index of 0 ([x])
-        console.log(calculus);
-        choseOperator = true; //this boolean will make us build the second number of our equation now
-    } else {
-        let secondValue = Number(secondNumber); //the string becomes a number
-        calculus.push(secondValue); //that number is pushed into the end of our array, which now has an index of 1 ([x, x])
-        let sum = calculus.reduce((accumulator, current) => accumulator + current, 0); //we get the sum of the numbers inside our array
-        calculus.splice(0, 2, sum); //since our array remains intact, we remove all previous values and insert the value of sum
-        firstNumber = String(sum); 
-        secondNumber = "";
-        document.getElementById("display").innerHTML = calculus;
-    }
-})
+const buttonZero = document.querySelector('#zero');
+buttonZero.addEventListener('click', () => {
+    insertNumber("0");
+});
 
+const buttonDot = document.querySelector('#dot');
+buttonDot.addEventListener('click', () => {
+    insertNumber(".");
+});
